3943db8b554f2f305692978a6421d8bf0d5b10bc
BlackLusterBob <Liammcnair@outlook.com>
2016-11-09 11:17:41 +0000

    Message pane scrool

--- template/chatScreen/body.php
+++ template/chatScreen/body.php
@@ -1,6 +1,3 @@
-<script>
-</script>
-
 <div class="messages-container">
     <div class="messages-inner">
         <div class="message">
@@ -140,6 +137,11 @@ jQuery(document).ready(function(){
     var windowHeight = window.innerHeight;
     jQuery('.messages-container').css('height', windowHeight-100);
     jQuery('.messages-container').css('max-height', windowHeight-100);
-    console.log(windowHeight);
+    console.log("ready");
+});
+jQuery(window).resize(function(){
+    var windowHeight = window.innerHeight;
+    jQuery('.messages-container').css('height', windowHeight-100);
+    jQuery('.messages-container').css('max-height', windowHeight-100);
 });
 </script>
