a93d427163691200ecfa6681c7fe9dfad82fc1a0
BlackLusterBob <Liammcnair@outlook.com>
2016-11-09 14:40:12 +0000

    Login handling

--- template/loginScreen/body.php
+++ template/loginScreen/body.php
@@ -2,7 +2,8 @@
     <div class="login-inner">
         <div class="login-logo"></div>
         <div class="login">
-            <form>
+
+            <form method="post" action="/php/login_auth.php">
                 <div id="login_username_error">
                     <p>Username must have more than 0 characters.</p>
                     <p>Username must have no more than 18 characters.</p>
@@ -10,9 +11,10 @@
                     <p>Username can also contain '-', '_' & '+'.</p>
                     <p>Username cannot contain spaces.</p>
                 </div>
-                <label>Pick Your Name:</label><input type="text" placeholder="Username..." id="login_username">
+                <label>Pick Your Name:</label><input type="text" placeholder="Username..." id="login_username" name="username">
                 <input type="submit" id="login_submit" value="Go" disabled>
             </form>
+            
         </div>
     </div>
 </div>
