fa37768ceae698deef799a9959cac98365feaa76
Carmen Valente <carmenvalente@rentalcars.com>
2016-11-09 14:38:37 +0000

    checks cookies for current username

--- js/main.js
+++ js/main.js
@@ -4,9 +4,7 @@ $(document).ready(function(){
 
 function getLatestMessages(){
     var mockData = '{"messages":[{"username":"bob", "content":"hello", "timestamp":"09/11/2016 10:38"},{"username": "fred", "content":"hi", "timestamp":"09/11/2016 10:38"},{"username":"jim", "content":"hey", "timestamp":"09/11/2016 10:38"}]}';
-    var currentUser = "bob";
-
-    var json = $.parseJSON(mockData, currentUser);
+    var json = $.parseJSON(mockData);
 
     refreshChat(json);
 
@@ -16,11 +14,11 @@ function getLatestMessages(){
     });**/
 }
 
-function refreshChat(data, currentUser) {
+function refreshChat(data) {
     if(typeof data == "object") {
         if (!(typeof data.messages == "undefined")) {
             for(var i = 0; i < data.messages.length; i++) {
-                if (data.messages[i].username == currentUser){
+                if (data.messages[i].username == $.cookie("username")){
                     $("#messagepane").append("<div class='message local'><div class='message-left'><p class='username'>" + data.messages[i].username + "</p> <p class='timestamp'>" + data.messages[i].timestamp + "</p></div><div class='message-right'><p class='message-content'>" + data.messages[i].content + "</div></div>");
 
                 } else {
