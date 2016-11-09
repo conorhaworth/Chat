3445cc0f3853751535509e7cdcc1df4041283116
BlackLusterBob <Liammcnair@outlook.com>
2016-11-09 10:41:57 +0000

    Message styling #1

--- template/chatScreen/body.php
+++ template/chatScreen/body.php
@@ -1,8 +1,28 @@
-<div class="message">
-    <div class="message-left">
-        Liam:
-    </div>
-    <div class="message-right">
-        Hello World!
+<div class="messages-container">
+    <div class="messages-inner">
+        <div class="message">
+            <div class="message-left">
+                Liam
+            </div>
+            <div class="message-right">
+                <p class="message-content">Hello World!</p>
+            </div>
+        </div>
+        <div class="message local">
+            <div class="message-left">
+                Conor
+            </div>
+            <div class="message-right">
+                <p class="message-content">Hi</p>
+            </div>
+        </div>
+        <div class="message">
+            <div class="message-left">
+                Carmen
+            </div>
+            <div class="message-right">
+                <p class="message-content">This is a rreally long and pointless message! asdfasdfbn jasdafsafsjbfs kjbfsjbkfskjblfskjlbfmskjblkjblsadlbfasdknfb vasdk  ad sdf asdfas</p>
+            </div>
+        </div>
     </div>
 </div>
