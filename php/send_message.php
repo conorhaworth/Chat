e5c961a0a33e9f3bc399b3f4c9d6ba8d0fe484b1
BlackLusterBob <Liammcnair@outlook.com>
2016-11-10 11:22:51 +0000

    Send message

--- php/send_message.php
+++ php/send_message.php
@@ -12,8 +12,20 @@
         return;
     }
     if(isset($message) && isset($username)){
-        echo '<script>console.log("M: '.$message.' U: '.$username.'")</script>';
-        return '<div class="return"><p>'.$message.'</p><p>'.$username.'</p></div>';
+        $data = ["content"  => $message];
+        $url = 'http://192.168.24.173/messages/'.$username;
+        
+        $ch = curl_init($url);
+        
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        
+        $response = curl_exec($ch);
+        echo $response;
+        return;
     }else{
         return;
     }
--- template/chatScreen/popup/settings.php
+++ template/chatScreen/popup/settings.php
@@ -1,4 +1,15 @@
 <ol class="list-grid">
-    <li class="list-grid-child"><ol class="list-grid-col"></ol></li>
-    <li class="list-grid-child"><ol class="list-grid-col"></ol></li>
+    <li class="modal-controller"><h1>Settings</h1><p class="modal-close">X</p></li>
+    <form>
+        <li class="list-grid-child">   
+            <ol class="list-grid-col">
+                <li class="settings"><input type="radio"><label>Default</label></li>
+            </ol>
+        </li>
+        <li class="list-grid-child">
+            <ol class="list-grid-col">
+            
+            </ol>
+        </li>
+    </form>
 </ol>
